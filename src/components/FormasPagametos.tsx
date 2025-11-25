a9dacb156b67b9d16e250776e4c0fde69df9a5da
vitorhugcunnha <vitor_hugo-cunha@estudante.sesisenai.org.br>
2025-11-25 19:46:57 -0300

    Feat: Adicionando mais formas de pagamentos estaticos.

--- src/components/FormasPagametos.tsx
+++ src/components/FormasPagametos.tsx
@@ -1,17 +1,36 @@
 export function FormasPagamentos(){
     return (
         <>
+       <div>
         <div className="bg-white p-[4px] rounded-[5px]">
-            <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-6 pr-6" src="src/assets/images/itau.png" />
-        </div>
-        <div className="bg-white p-[4px] rounded-[5px]">
-            <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-2 pr-2" src="src/assets/images/master.png" />
-        </div>
-        <div className="bg-white p-[4px] rounded-[5px]">
-            <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4 pt-2 pb-2" src="src/assets/images/pix.png" />
-        </div>
-        <div className="bg-white p-[4px] rounded-[5px]">
-            <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4" src="src/assets/images/visa.png" />
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-6 pr-6" src="src/assets/images/itau.png" />
+            </div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-2 pr-2" src="src/assets/images/master.png" />
+            </div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4 pt-2 pb-2" src="src/assets/images/pix.png" />
+            </div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4" src="src/assets/images/visa.png" />
+            </div>
+       </div>
+        
+
+
+        <div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-6 pr-6" src="src/assets/images/Caixa.png" />
+            </div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-2 pr-2" src="src/assets/images/Hipercard.png" />
+            </div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4 pt-2 pb-2" src="src/assets/images/Nubank.png" />
+            </div>
+            <div className="bg-white p-[4px] rounded-[5px]">
+                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4" src="src/assets/images/Satander.png" />
+            </div>
         </div>
         </>
     )
