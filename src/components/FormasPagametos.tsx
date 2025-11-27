efde7733f91e3f92ab2b13315c4e42798b3a81d4
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2025-11-26 22:24:02 -0300

    Feat: Finalizado as 4 formas de pagamentos, proximo passo fazer os card de planos

--- src/components/FormasPagametos.tsx
+++ src/components/FormasPagametos.tsx
@@ -1,37 +1,38 @@
 export function FormasPagamentos(){
     return (
         <>
-       <div>
-        <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-6 pr-6" src="src/assets/images/itau.png" />
-            </div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-2 pr-2" src="src/assets/images/master.png" />
-            </div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4 pt-2 pb-2" src="src/assets/images/pix.png" />
-            </div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4" src="src/assets/images/visa.png" />
-            </div>
-       </div>
-        
+            <div className="grid grid-cols-4 gap-10">
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-6" src="src/assets/images/itau.png" />
+                </div>
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-2" src="src/assets/images/master.png" />
+                </div>
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-6 py-2" src="src/assets/images/pix.png" />
+                </div>
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-4" src="src/assets/images/visa.png" />
+                </div>
 
 
-        <div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-6 pr-6" src="src/assets/images/Caixa.png" />
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-6" src="src/assets/images/Caixa.png" />
+                </div>
+
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-2" src="src/assets/images/Hipercard.png" />
+                </div>
+
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] px-8 py-2" src="src/assets/images/Nubank.png" />
+                </div>
+
+                <div className="bg-white p-[4px] rounded-[5px]">
+                    <img className="h-8 bg-white border-[2px] border-[#c7c7c7] rounded-[5px] pl-6 pr-8" src="src/assets/images/Satander.png" />
+                </div>
             </div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-2 pr-2" src="src/assets/images/Hipercard.png" />
-            </div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4 pt-2 pb-2" src="src/assets/images/Nubank.png" />
-            </div>
-            <div className="bg-white p-[4px] rounded-[5px]">
-                <img className="h-8 bg-[#ffffff] border-[2px] border-[#c7c7c7] rounded-[5px] pl-4 pr-4" src="src/assets/images/Satander.png" />
-            </div>
-        </div>
-        </>
+            </>
+
     )
 }
