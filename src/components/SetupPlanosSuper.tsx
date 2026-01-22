0134a23d913d7967cc83b0951ea1b4f2518765f3
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2026-01-21 22:00:49 -0300

    perf: Organizando o setup dos planos super.

--- src/components/SetupPlanosSuper.tsx
+++ src/components/SetupPlanosSuper.tsx
@@ -1,7 +1,24 @@
+import { PlanosSuper } from "./PlanosSuper"
+import { planos } from "../data/planos";
+import { Plan } from "../types/types";
+
 export function SetupPlanosSuper() {
     return (
         <>
-        
+            <div className="w-full h-min bg-[#1f2020]">
+                <div className="flex items-center justify-center pb-20 pt-20">
+                    <h1 className="text-white font-bold text-4xl pt-5">Planos Super</h1>
+                </div>
+            </div>
+            <div className="w-full bg-[#1f2020] flex justify-evenly">
+                <div className="max-w-7xl flex flex-col md:flex-row gap-8 px-8 mb-32">
+                    {planos.map((plano: Plan, index: number) => (
+                        <PlanosSuper
+                        key={index}
+                        plan={plano}/>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,9 @@
-import {planos} from "../data/planos.ts";
 import NavBar from "../components/general/NavBar";
 import { TableSection } from "../components/TableSection";
 import { MenuHome } from "../components/MenuHome";
 import {PagamentosSetup} from "../components/PagamentosSetup";
-import { PlanosSuper } from "../components/PlanosSuper";
 import { FooterHome } from "../components/FooterHome";
+import { SetupPlanosSuper } from "../components/SetupPlanosSuper";
 
 export default function Home() {
   return (
@@ -14,27 +13,12 @@ export default function Home() {
         <MenuHome />
           <TableSection />
             <PagamentosSetup />
-        <div>
-          <div className="w-full h-min bg-[#1f2020]">
-            <div className="flex items-center justify-center pb-20 pt-20">
-                <h1 className="text-white font-bold text-4xl pt-5">Planos Super</h1>
-            </div>
-          </div>
-            <div className="w-full bg-[#1f2020] flex justify-evenly">
-              <div className="max-w-7xl flex flex-col md:flex-row gap-8 px-8 mb-32">
-                {planos.map((plano, index) => (
-                  <PlanosSuper
-                    key={index}
-                    plan={plano}/>
-                ))}
-              </div>
-            </div>
+          <SetupPlanosSuper />
             <div className="w-full h-min bg-[#000000]">
               <footer className="flex justify-evenly items-center h-32">
                 <FooterHome />
               </footer>
             </div>
-        </div>
       </main>
     </>
   );
