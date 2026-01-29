c446b28b313ae7b14949d2563476813d99f966c1
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2026-01-28 21:10:58 -0300

    style: Melhorando footer

--- src/components/footerHome.tsx
+++ src/components/footerHome.tsx
@@ -1,13 +1,21 @@
+import {Instagram, Linkedin} from 'lucide-react';
+
 export function FooterHome() {
     return (
-    <>
-        <div>
-            <img className="h-[60px]" src="src/assets/images/Logo.png"/>
+        <div className="w-full h-[120px] bg-black">
+            <div className="flex items-center gap-2">
+                <img className="h-[60px]" src="src/assets/images/Logo.png"/>
+                <div className="flex flex-col items-center">
+                    <span className="text-white text-sm">Feito por Vitor Hugo da Cunha </span>
+                    <span className="text-white text-sm">© 2026 Bryar Music.</span>
+                </div>
+            </div>
+            <div>
+                <div className="flex gap-4 mt-2">
+                    <Instagram className="text-white cursor-pointer hover:text-pink-500 transition" />
+                    <Linkedin className="text-white cursor-pointer hover:text-blue-500 transition" />
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col">
-            <span className="text-white">Todos Direitos Autorais de Vitor Hugo da Cunha</span>
-            <span className="text-white">© 2024 Bryar Music.</span>
-        </div>
-    </>
-    )        
+    )
 }
