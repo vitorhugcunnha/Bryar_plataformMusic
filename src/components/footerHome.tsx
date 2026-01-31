942ec7bc346b91ff5e9083733d295d29ddd9ff47
Vitcholas2000 <vitor_hugo-cunha@estudante.sesisenai.org.br>
2026-01-30 21:58:33 -0300

    style: Add novas duas icons de Likedin e Instagram, linkando minhas redes sociais

--- src/components/footerHome.tsx
+++ src/components/footerHome.tsx
@@ -1,21 +1,19 @@
-import {Instagram, Linkedin} from 'lucide-react';
+import { Instagram, Linkedin } from "lucide-react";
 
 export function FooterHome() {
     return (
-        <div className="w-full h-[120px] bg-black">
-            <div className="flex items-center gap-2">
-                <img className="h-[60px]" src="src/assets/images/Logo.png"/>
+        <div className="w-full h-[200px] bg-black flex flex-col justify-evenly items-center">
+            <div className="flex items-center gap-10 md:gap-72">
+                <img className="h-[50px] md:h-[60px]" src="src/assets/images/Logo.png"/>
                 <div className="flex flex-col items-center">
-                    <span className="text-white text-sm">Feito por Vitor Hugo da Cunha </span>
+                    <span className="text-white text-sm">Feito por Vitor Hugo da Cunha</span>
                     <span className="text-white text-sm">© 2026 Bryar Music.</span>
-                </div>
-            </div>
-            <div>
-                <div className="flex gap-4 mt-2">
-                    <Instagram className="text-white cursor-pointer hover:text-pink-500 transition" />
-                    <Linkedin className="text-white cursor-pointer hover:text-blue-500 transition" />
+                    <div className="flex gap-6 md:gap-8 md:pt-2">
+                        <a href="https://www.instagram.com/vitorcunha12/" target="_blank" rel="noopener noreferrer"><Instagram className="text-white cursor-pointer transition-all duration-500 ease-out hover:-translate-y-0.5 hover:text-pink-500" /></a>
+                        <a href="https://www.linkedin.com/in/vitor-hugo-da-cunha-8a429830a/" target="_blank" rel="noopener noreferrer"><Linkedin className="text-white cursor-pointer transition-all duration-500 ease-out hover:-translate-y-0.5 hover:text-blue-500" /></a>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
